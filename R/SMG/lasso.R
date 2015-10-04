0ef39fcd748dede5b337c7b66de03974ccf0a430
Swapnil Jamthe <swapnil@Swapnils-MacBook-Pro.local>
2015-10-04 18:53:31 +0530

    first run lasso

--- R/SMG/lasso.R
+++ R/SMG/lasso.R
@@ -4,8 +4,8 @@ library(dplyr)
 library(caret)
 library(randomForest)
 library(imputeR)
-library(AUC)
 library(glmnet)
+library(AUC)
 setwd("/Users/swapnil/work/Kaggle/out/SMR")
 cat("loading data")
 train<-read.csv("train.csv",stringsAsFactors=FALSE)
@@ -178,6 +178,7 @@ cvData<-train_proc[-trainingIndex,]
 trainingData$target = as.numeric(trainingData$target)
 cvData$target = as.numeric(cvData$target) 
 
+metric="lasso"
 fullModelMat<-model.matrix(~.,data = train_proc)
 preProc<-preProcess(fullModelMat)
 processedFullModelMat<-predict(preProc,fullModelMat)
@@ -185,9 +186,83 @@ modelMatTrain<-model.matrix(target~.,data = trainingData)
 modelMatCv<-model.matrix(~.,data = cvData)
 modelMatTestFinal<-model.matrix(~.,data = test_proc)
 
-lassoModel<-cv.glmnet(modelMatTrain,trainingData$target,alpha = 1)
-plot(lassoModel)
-bestlam<-lassoModel$lambda
+#lassoModel<-cv.glmnet(modelMatTrain,trainingData$target,alpha = 1)
+#plot(lassoModel)
+#bestlam<-lassoModel$lambda
+#lambda.min : 9.153017e-05
+#lambda.1se : 0.0002320623
 
+lassoModel<-glmnet(modelMatTrain,trainingData$target,alpha = 1,lambda = 9.153017e-05)
+
+nonZeroCols<-dimnames(coef(lassoModel))[[1]][coef(lassoModel)[,1] != 0]
+
+modelMatTrain<-modelMatTrain[,nonZeroCols]
+modelMatCv<-modelMatCv[,nonZeroCols]
+modelMatTestFinal<-modelMatTestFinal[,nonZeroCols]
+
+obs<-data.frame(iter=integer(0),rocscore=numeric(0),rocscoreTrain=numeric(0),ntree=integer(0),depth=integer(0),eta=integer(0))
+
+trials<-data.frame(ntree=integer(0),depth=integer(0),eta=integer(0))
+trials<-rbind(trials,data.frame(ntree=2,depth=2,eta=0.1502))
+
+trials<-rbind(trials,data.frame(ntree=50,depth=6,eta=0.15035))
+trials<-rbind(trials,data.frame(ntree=50,depth=6,eta=0.15036))
+trials<-rbind(trials,data.frame(ntree=50,depth=6,eta=0.15037))
+trials<-rbind(trials,data.frame(ntree=50,depth=6,eta=0.15038))
+trials<-rbind(trials,data.frame(ntree=50,depth=6,eta=0.15039))
+trials<-rbind(trials,data.frame(ntree=50,depth=6,eta=0.15041))
+trials<-rbind(trials,data.frame(ntree=50,depth=6,eta=0.15042))
+trials<-rbind(trials,data.frame(ntree=50,depth=6,eta=0.15043))
+trials<-rbind(trials,data.frame(ntree=50,depth=6,eta=0.15044))
+
+
+cat("iter,rocscore,rocscoreTrain,ntree,depth,eta,metric\n",file="obs/lasso/obs.txt",append=TRUE)
+
+library(xgboost)
+iter = 0
+bestR<--1;
+bestIter<--1;
+for(it in 1:nrow(trials))
+{
+  iter = iter + 1;
+  currEta<-trials[it,"eta"]
+  currNtree<-trials[it,"ntree"]
+  currDepth<-trials[it,"depth"]
+  
+  currTrainMat<-modelMatTrain
+  currCvMat<-modelMatCv
+  currTestMat<-modelMatTestFinal
+  
+  modelBoost<-xgboost(data = currTrainMat, label = trainingData$target, max.depth = currDepth, eta = currEta, nthread = 2, nround = currNtree, objective = "binary:logistic",verbose=0)
+  predBoost<-predict(modelBoost,currTestMat)
+  predBoostFrame<-data.frame(Id=test$ID,target=predBoost)
+  predCv<-predict(modelBoost,currCvMat)
+  r<-AUC::auc(roc(predictions = predCv,labels = factor(cvData$target)))
+  predTrain<-predict(modelBoost,currTrainMat)
+  rtrain<-AUC::auc(roc(predictions = predTrain,labels = factor(trainingData$target)))
+  
+  obs<-rbind(obs,data.frame(iter=iter,rocscore=r,rocscoreTrain=rtrain,ntree=currNtree,depth=currDepth,eta=currEta))
+  currIterLog<-paste0("iter=",iter,",rocscore=",r,",rocscoreTrain=",rtrain,",ntree=",currNtree,",depth=",currDepth,",eta=",currEta)
+  print(currIterLog)
+  
+  csvLog<-paste0(iter,r,rtrain,currNtree,currDepth,currEta,metric,"\n")
+  cat(csvLog,file="obs/lasso/obs.txt",append=TRUE)
+  
+  
+  if(bestR < r)
+  {
+    
+    print(paste0("Best till now ",currIterLog,":: lastBest=",bestR,",lastBestIter=",bestIter))
+    
+    bestR<-r
+    bestIter<-iter
+    
+    print(paste0("Best till now ",currIterLog))
+    
+    write.table(predBoostFrame,file = paste0("predictions/lasso/predBoost","_",currNtree,"_",currDepth,"_",currEta,"_",iter),quote = FALSE,sep = ",",row.names = FALSE)
+    
+  }
+  
+}
 
 
