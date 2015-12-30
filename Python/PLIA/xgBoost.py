8ad5e935731c4af30a44d072676aa269d8acee42
Swapnil Jamthe <swapnil@Swapnils-MacBook-Pro.local>
2015-12-31 01:05:59 +0530

    Added code to split data into train and cv

--- Python/PLIA/xgBoost.py
+++ Python/PLIA/xgBoost.py
@@ -3,14 +3,17 @@ import pandas as pd
 from sklearn.feature_extraction import DictVectorizer as DV
 from sklearn.cross_validation import train_test_split
 import os
-os.chdir("/Users/swapnil.jamthe/work/Kaggle/out/PLIA")
+os.chdir("/Users/swapnil/work/Kaggle/out/PLIA")
 print "Hello"
 data = pd.read_csv("trans_train.csv")
 test = pd.read_csv("trans_test.csv",na_values="NA")
 d = DV(sparse = True)
-print test["Medical_History_15"]
+
 data = data.fillna(-9999)
 test = test.fillna(-9999)
-print test["Medical_History_15"]
 
-ftTrain = d.fit_transform(data.T.to_dict().values())
+xData = data.drop("Response",axis=1)
+print xData.columns
+
+trainData, cvData, yTrain, yCv = train_test_split(xData,data["Response"], test_size=0.8, random_state=42)
+ftTrain = d.fit_transform(trainData.T.to_dict().values())
