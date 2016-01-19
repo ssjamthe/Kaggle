4cebee69daec022e4cc7c86d9644a7a981d0b788
Swapnil Jamthe <swapnil@Swapnils-MacBook-Pro.local>
2016-01-20 00:23:42 +0530

    Studying why cv pred is bad

---
+++ Python/PLIA/test1.py
@@ -0,0 +1,23 @@
+import xgboost as xgb
+import pandas as pd
+from sklearn.feature_extraction import DictVectorizer as DV
+from sklearn.cross_validation import train_test_split
+from skll import kappa
+import os
+
+
+#outputDf = pd.DataFrame({"first" : [],"second" : []})
+#outputDf = outputDf.append(pd.DataFrame({"first" : [2],"second" : [3]}))
+os.chdir("/Users/swapnil/work/Kaggle/out/PLIA")
+print "hello"
+
+
+df1Data = {"col1" : [1,2,3],"col2" : ["swap","kals","bang"]}
+df1 = pd.DataFrame(data = df1Data)
+
+d = DV(sparse = True)
+d1 = d.fit_transform(df1.T.to_dict().values())
+d1
+
+df1Data = {"col1" : [1,20,30],"col2" : ["swap","kals1","bang"],"col3":[33,44,55]}
+df1 = pd.DataFrame(data = df1Data)
