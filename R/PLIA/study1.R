dcaa0ee47c575993d33c71649843d98e023cdc00
Swapnil Jamthe <swapnil@Swapnils-MacBook-Pro.local>
2016-02-04 23:23:04 +0530

    Adding medical keyword fields

---
+++ R/PLIA/study1.R
@@ -0,0 +1,45 @@
+set.seed(1988)
+
+library(dplyr)
+
+transDataByMkSum<-function(data)
+{
+  #print(names(data))
+  data$mkSum = rep(0,nrow(data))
+  for(i in 1:nrow(data))
+  {
+    mkSum = 0
+    for(colName in names(data))
+    {
+      if((grepl("Medical_Keyword",colName))[1] == TRUE)
+      {
+        #print(paste0(class(data[i,colName]),colName,data[i,colName]))
+        if(!is.na(data[i,colName]))
+        {
+          mkSum = mkSum + data[i,colName]
+        }
+      }
+    }
+    
+    data[i,"mkSum"] = mkSum
+  }
+  
+  
+  data<-select(data, -contains("Medical_Keyword"))
+  
+  return(data)
+}
+
+setwd("/Users/swapnil/work/Kaggle/out/PLIA")
+data<-read.csv("trans_train.csv")
+data<-transDataByMkSum(data)
+
+
+write.table(data,file = "trans_train_sumMK.csv",quote = FALSE,sep = ",",row.names = FALSE)
+
+dataTest<-read.csv("trans_test.csv")
+dataTest<-transDataByMkSum(dataTest)
+
+
+write.table(dataTest,file = "trans_test_sumMK.csv",quote = FALSE,sep = ",",row.names = FALSE)
+
